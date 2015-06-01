baada5c23b46ad4bea6207773f5e069449d9a32d
jaredmixpanel <jared@mixpanel.com>
2015-06-01 13:31:22 +0200

    initial commit

--- MPTest/AppDelegate.m
+++ MPTest/AppDelegate.m
@@ -7,6 +7,10 @@
 //
 
 #import "AppDelegate.h"
+#import <Mixpanel/Mixpanel.h>
+#import <AdSupport/AdSupport.h>
+
+#define MIXPANEL_TOKEN @"4273def7ede898606e72925316938d92"
 
 @interface AppDelegate ()
 
@@ -17,6 +21,50 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+//    NSString *data = @"event_bindings";
+//    NSString *token = @"4273def7ede898606e72925316938d92";
+//    NSString *filename = [NSString stringWithFormat:@"mixpanel-%@-%@.plist", token, data];
+//    NSString *filePath = [[NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSUserDomainMask, YES) lastObject]
+//            stringByAppendingPathComponent:filename];
+//    
+//    if ([[NSFileManager defaultManager] fileExistsAtPath:filePath]) {
+//        NSError *error;
+//        BOOL removed = [[NSFileManager defaultManager] removeItemAtPath:filePath error:&error];
+//        if (!removed) {
+//            NSLog(@"%@ unable to remove archived file at %@ - %@", self, filePath, error);
+//        }
+//    }
+//    
+//    if (![NSKeyedArchiver archiveRootObject:[NSSet set] toFile:filePath]) {
+//        NSLog(@"%@ unable to archive tracking events data", self);
+//    }
+    
+    // Initialize the library with your
+    // Mixpanel project token, MIXPANEL_TOKEN
+    [Mixpanel sharedInstanceWithToken:MIXPANEL_TOKEN launchOptions:launchOptions];
+    if (launchOptions[UIApplicationLaunchOptionsRemoteNotificationKey]) {
+        [[Mixpanel sharedInstance] trackPushNotification:launchOptions[UIApplicationLaunchOptionsRemoteNotificationKey]];
+    }
+    NSString * distinctID = [Mixpanel sharedInstance].distinctId;
+    NSString * uuid = [[[UIDevice currentDevice] identifierForVendor] UUIDString];
+    NSString *ifa = [ASIdentifierManager sharedManager].advertisingIdentifier.UUIDString;
+    
+    [[Mixpanel sharedInstance] identify:distinctID];
+    [Mixpanel sharedInstance].showSurveyOnActive = NO;
+    [Mixpanel sharedInstance].checkForSurveysOnActive = NO;
+    
+    UIUserNotificationSettings *notificationSettings = [UIUserNotificationSettings settingsForTypes:(UIUserNotificationTypeBadge |
+                                                                                                     UIUserNotificationTypeSound |
+                                                                                                     UIUserNotificationTypeAlert)
+                                                                                         categories:UIUserNotificationActionContextDefault];
+    [[Mixpanel sharedInstance].people setOnce:@{@"$created":[NSDate date]}];
+    
+    [[Mixpanel sharedInstance].people append:@{@"Brand Names":@[@"Brand A", @"Brand B"]}];
+    [[Mixpanel sharedInstance].people union:@{@"Brand Names":@[@"Brand A",@"Brand B",@"Brand C"]}];
+    [[UIApplication sharedApplication] registerUserNotificationSettings:notificationSettings];
+    
+
     return YES;
 }
 
@@ -40,6 +88,32 @@
 
 - (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    [[Mixpanel sharedInstance] track:@"applicationWillTerminate"];
+    [[Mixpanel sharedInstance] archive];
+    // NSLog(@"app delegate will terminate");
 }
 
+- (void)application:(UIApplication *)application didRegisterUserNotificationSettings:(UIUserNotificationSettings *)notificationSettings
+{
+    //register to receive notifications
+    [application registerForRemoteNotifications];
+}
+
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
+{
+    Mixpanel *mp = [Mixpanel sharedInstance];
+    [mp.people addPushDeviceToken:deviceToken];
+    [mp identify:mp.distinctId];
+}
+
+- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error
+{
+    NSLog(@"%@",error);
+}
+
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo
+{
+    
+    [[Mixpanel sharedInstance] trackPushNotification:userInfo];
+}
 @end
--- MPTest/ViewController.m
+++ MPTest/ViewController.m
@@ -5,23 +5,109 @@
 //  Created by Jared McFarland on 1/21/15.
 //  Copyright (c) 2015 Jared McFarland. All rights reserved.
 //
-
+#import <CoreText/CoreText.h>
 #import "ViewController.h"
+#import "AppDelegate.h"
+#import <Mixpanel/Mixpanel.h>
+#import <Mixpanel/MPTweakInline.h>
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIButton *button;
+- (IBAction)fire:(id)sender;
+// @property (weak, nonatomic) IBOutlet UIButton *button2;
+@property (strong, nonatomic) Mixpanel *mixpanel;
+@property (strong, nonatomic) NSArray *number;
+
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *barButton;
+
 
 @end
 
 @implementation ViewController
 
+-(Mixpanel *)mixpanel
+{
+    if (!_mixpanel) {
+        _mixpanel = [Mixpanel sharedInstance];
+    }
+    return _mixpanel;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+   
+    UILabel *label = [[UILabel alloc] init];
+    BOOL l1 = MPTweakValue(@"Label 1",NO);
+    if (l1) {
+        label.text = @"TRUE";
+    } else if (!l1)
+    {
+        label.text = @"FALSE";
+    }
+    [label setFrame:CGRectMake(self.view.frame.size.width/2, self.view.frame.size.height/2, 200, 100)];
+    [label sizeToFit];
+    [self.view addSubview:label];
+
+    UILabel *tweakLabel = [[UILabel alloc] init];
+    BOOL l2 = MPTweakValue(@"Label 2",YES);
+    if (l2) {
+        tweakLabel.text = @"TRUE";
+    } else if (!l2)
+    {
+        tweakLabel.text = @"FALSE";
+    }
+    [tweakLabel setFrame:CGRectMake(self.view.frame.size.width/4, self.view.frame.size.height/4, 200, 100)];
+    [tweakLabel sizeToFit];
+    [self.view addSubview:tweakLabel];
+    
+//    UILabel *tweakLabel2 = [[UILabel alloc] init];
+//    //tweakLabel.text = @"INT = 0";
+//    tweakLabel2.text = MPTweakValue(@"TweakLabel Text",@"SHIT");
+//    [tweakLabel2 setFrame:CGRectMake(self.view.frame.size.width/4, self.view.frame.size.height/4, 200, 100)];
+//    [tweakLabel2 sizeToFit];
+//    [self.view addSubview:tweakLabel2];
+    
+    
+    self.number = @[@0];
+    
+    [self.mixpanel registerSuperProperties:@{@"Events Sent": @0}];
+
+    //[self.mixpanel.people set: @{@"List":@[@"item1",@"item2",@"item3"]}];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)fire:(id)sender {
+    
+    NSString *event = @"Event Name";
+    
+    NSMutableDictionary *mutableProperties = [NSMutableDictionary dictionary];
+    
+    [mutableProperties setObject:@"AB" forKey:@"mp_country_code"];
+    
+    [mutableProperties setObject:@"Jared City" forKey:@"$city"];
+    
+    [mutableProperties setObject:@"McFarlandia" forKey:@"$region"];
+    
+    [[Mixpanel sharedInstance] track:event properties:mutableProperties];
+    
+    NSMutableDictionary *superProps = [[self.mixpanel currentSuperProperties] mutableCopy];
+    superProps[@"propToIncrement"] = @([superProps[@"propToIncrement"] intValue] + 1);
+    [self.mixpanel registerSuperProperties:superProps];
+    
+}
+- (void)fire2:(id)sender {
+    NSLog(@"FIRE2!");
+    [self.mixpanel.people set:@"People Property" to:@"hello"];
+    [self.mixpanel.people set:@{@"$first_name":@"Jared",@"$last_name":@"McFarland",@"$email":@"jared@mixpanel.com"}];
+    
+    [self.mixpanel.people trackCharge:@200 withProperties:@{@"$time":[NSDate date]}];
+    //[self.mixpanel flush];
 }
 
+- (void)incrementEventsCountBy:(int)amount
+{
+    NSNumber *eventsSentCount = [self.mixpanel currentSuperProperties][@"Events Sent"];
+    eventsSentCount = @(eventsSentCount.intValue + amount);
+    [self.mixpanel registerSuperProperties:@{@"Events Sent": eventsSentCount}];
+}
 @end
